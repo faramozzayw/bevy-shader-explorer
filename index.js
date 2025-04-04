22516bf355e3d15f98b71455091d2c143fcf0463
faramozzayw <faramo_zayw@protonmail.com>
2025-04-05 00:24:52 +0300

    init

--- index.js
+++ index.js
@@ -2,19 +2,18 @@
 
 const fs = require("node:fs");
 const path = require("node:path");
+const { exec } = require("node:child_process");
 const Handlebars = require("handlebars");
 
-Handlebars.registerHelper("eq", function (a, b) {
-  return a === b;
-});
+const wgpuTypes = JSON.parse(fs.readFileSync("./wgpu-types.json", "utf-8"));
 
-Handlebars.registerHelper("neq", function (a, b) {
-  return a !== b;
-});
+Handlebars.registerHelper("eq", (a, b) => a === b);
+Handlebars.registerHelper("neq", (a, b) => a !== b);
 
 const TEMPLATE_SOURCE = fs.readFileSync("template.hbs", "utf-8");
 const FUNCTION_PATTERN =
   /(@[^;]*\s+)?(vertex|fragment|compute\s+)?fn\s+([a-zA-Z0-9_]+)\s*\(([^)]*)\)(\s*->\s*([^{]*))?/g;
+const OUTPUT_DIR = "./outputs";
 
 function extractWGSLFunctions(wgslCode) {
   const normalizedCode = wgslCode.replace(/\r\n/g, "\n");
@@ -58,13 +57,11 @@ function extractWGSLFunctions(wgslCode) {
 
     // Handle single-line comments
     else if (line.startsWith("//")) {
-      if (!lineComments[i + 1]) {
-        lineComments[i + 1] = line.substring(2).trim();
-      } else {
-        lineComments[i + 1] += "\n" + line.substring(2).trim();
-      }
+      lineComments[i + 1] = line
+        .substring(line.startsWith("///") ? 3 : 2)
+        .trim();
     } else {
-      // If we encounter a non-comment line after collecting comments but before a function,
+      // NOTE: If we encounter a non-comment line after collecting comments but before a function,
       // store these comments for the next line
       if (commentBuffer.length > 0) {
         lineComments[i + 1] = commentBuffer.join("\n");
@@ -80,7 +77,6 @@ function extractWGSLFunctions(wgslCode) {
 
   while ((match = FUNCTION_PATTERN.exec(fullCode)) !== null) {
     const attributes = match[1] ? match[1].trim() : "";
-    const visibility = match[2] ? match[2].trim() : "";
     const name = match[3];
     const params = match[4]
       .split(",")
@@ -88,7 +84,6 @@ function extractWGSLFunctions(wgslCode) {
       .filter((p) => p);
     const returnType = match[6] ? match[6].trim() : "void";
 
-    // Find line number of the function
     const positionInCode = match.index;
     const codeBeforeMatch = fullCode.substring(0, positionInCode);
     const lineNumber = codeBeforeMatch.split("\n").length;
@@ -96,14 +91,14 @@ function extractWGSLFunctions(wgslCode) {
     const comments = getFunctionComments(lineNumber, lineComments);
     const formattedParams = getFunctionParams(params);
 
-    comments.reverse();
+    const returnTypeLink = wgpuTypes?.[returnType.split("<")[0]] ?? null;
 
     functions.push({
       attributes,
-      visibility,
       name,
       params: formattedParams,
       returnType,
+      returnTypeLink,
       comment: comments.join("\n"),
     });
 
@@ -117,9 +112,13 @@ function getFunctionParams(params) {
   return params.map((param) => {
     const parts = param.split(":");
 
+    const type = parts[1].trim();
+    const maybeGenericType = type.split("<")[0];
+
     return {
       name: parts[0].trim(),
-      type: parts[1].trim(),
+      type,
+      typeLink: wgpuTypes?.[maybeGenericType] ?? null,
     };
   });
 }
@@ -139,6 +138,8 @@ function getFunctionComments(lineNumber, lineComments) {
     comments.push(lineComments[currentLine]);
   }
 
+  comments.reverse();
+
   return comments;
 }
 
@@ -152,7 +153,27 @@ function processWGSLFile(wgslFilePath) {
   const output = generateFunctionDocsHTML(functions);
   const filename = path.parse(wgslFilePath).name;
 
-  fs.writeFileSync(`./outputs/${filename}.html`, output, "utf-8");
+  fs.mkdirSync(OUTPUT_DIR, { recursive: true });
+  fs.writeFileSync(path.join(OUTPUT_DIR, `${filename}.html`), output, {
+    encoding: "utf-8",
+  });
 }
 
-processWGSLFile("./wgsls/ssr.wgsl");
+const GREP_WGSL = `grep -rl --include="*.wgsl" .`;
+
+exec(GREP_WGSL, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`❌ Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`⚠️ stderr: ${stderr}`);
+    return;
+  }
+
+  const filePaths = stdout.trim().split("\n");
+
+  for (const filePath of filePaths) {
+    processWGSLFile(filePath);
+  }
+});
